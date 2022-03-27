1e08c54108382582f0a9a1d3b168dafd851bcdb6
Idris Paragulgov <58070651+ipa9@users.noreply.github.com>
2022-03-28 00:05:19 +0400

    Update Figure.java

--- Figure.java
+++ Figure.java
@@ -1,5 +1,29 @@
 /*
-
+Given a class Point, an abstract class Figure, skeletons of classes Triangle, Quadrilateral,
+Circle, implement the following.
+Make Triangle, Quadrilateral, Circle extend Figure class.
+Implement methods in Triangle, Quadrilateral, Circle:
+• Constructors with following parameters:
+o Triangle - three vertices (points) as parameters.
+o Quadrilateral - four vertices (points) as parameters.
+o Circle - point of the center and double value of the radius.
+Ensure figures are not degenerative.
+All of them must have non-zero area.
+Quadrilateral is also must be convex.
+If a figure is not good, throw an IllegalArgumentException.
+Note: A non-degenerative convex quadrilateral is divided into four non-degenerative
+triangles by its diagonals. Note: double calculations are not completely accurate, use
+error delta, where applies.
+• public Point centroid()
+Return the centroid of the figure.
+Centroid refers to center of mass of the plain figure, not the baricenter.
+In other words it should be "area centroid".
+• public boolean isTheSame(Figure figure)
+Two figures are considered to be the same only:
+o if they have the same type
+o and if they coincide (e.g. have same vertices).
+Note: order of the vertices has not to be the same.
+Note: double calculations are not completely accurate, use error delta, where applies.
 */
 
 import java.util.Scanner;
