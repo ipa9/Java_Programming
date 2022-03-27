da72276add5d4fad2f46709126f46cebb08d1117
Idris Paragulgov <58070651+ipa9@users.noreply.github.com>
2022-03-27 23:44:02 +0400

    Create enumTest.java

---
+++ enum/enumTest.java
@@ -0,0 +1,10 @@
+public class Enum {
+	public static void main(String[] args) {
+	
+	for (Fruits fruits : Fruits.values()) {
+		System.out.println(fruits);
+		System.out.println(Fruits.calories);
+	}
+
+	}
+}
